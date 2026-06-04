207686d1fe4b1ffc9dfa6fb8b25290348a804907
Mariano Ruiz <mariano.ruiz@upm.es>
2026-06-04 11:42:48 +0200

    Fix version windows installaer

--- vc-install/windows/installer2.ps1
+++ vc-install/windows/installer2.ps1
@@ -187,11 +187,11 @@ function Install-CmsisPackIfMissing {
 
 # --- CMSIS Packs ---
 Write-Host "[PASO 9/10] Instalando CMSIS Packs..." -ForegroundColor Magenta
-Install-CmsisPackIfMissing -Vendor "ARM"  -Pack "CMSIS"         -Version "5.8.0"
-Install-CmsisPackIfMissing -Vendor "Keil" -Pack "STM32F4xx_DFP" -Version "2.15.0"
-Install-CmsisPackIfMissing -Vendor "ARM"  -Pack "CMSIS-Driver"  -Version "2.6.1"
-#Install-CmsisPackIfMissing -Vendor "ARM"  -Pack "CMSIS-RTX"     -Version "5.5.2 "
-Install-CmsisPackIfMissing -Vendor "Keil" -Pack "MDK-Middleware" -Version "7.13.0"
+Install-CmsisPackIfMissing -Vendor "ARM"  -Pack "CMSIS"         -Version "6.3.0"
+Install-CmsisPackIfMissing -Vendor "Keil" -Pack "STM32F4xx_DFP" -Version "2.17.1"
+Install-CmsisPackIfMissing -Vendor "ARM"  -Pack "CMSIS-Driver"  -Version "2.10.0"
+Install-CmsisPackIfMissing -Vendor "ARM"  -Pack "CMSIS-RTX"     -Version "5.9.1"
+Install-CmsisPackIfMissing -Vendor "Keil" -Pack "MDK-Middleware" -Version "8.1.0"
 Write-Host "[INFO] Lista de packs instalados:" -ForegroundColor Yellow
 cpackget list
 
